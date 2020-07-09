ad5ca2c85b0baa1664167574a6a968a90e12b6dd
Sebastian <jsruiz7411@misena.edu.co>
2020-07-09 15:19:41 -0500

    script:Se elimina un archivo

--- Página HTML CSS JS/codigo2.js
+++
@@ -1,7 +0,0 @@
-function validar(form1){
-    console.log(" ");
-    console.log("Validando Forma");
- var usuario = forma.usuario;
- console.log("El Usuario es:" + usuario.value);
- return false;
-}
